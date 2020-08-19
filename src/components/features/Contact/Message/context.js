86ed63eb88b1c6a1f565afcea3879e6bbc9e82f1
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-19 03:15:54 -0700

    :cyclone: Add initial value of context

--- src/components/features/Contact/Message/context.js
+++ src/components/features/Contact/Message/context.js
@@ -1,5 +1,15 @@
 import React from "react"
 
-const messageContext = React.createContext({ isNewMessage: false })
+const messageDefault = {
+    contactName: "username",
+    contactPhoto: {
+        url: null,
+        alt: null
+    },
+    time: "00:00",
+    text: "",
+    markedAsRead: false
+}
+const messageContext = React.createContext(messageDefault)
 
-export default messageContext 
+export default messageContext
