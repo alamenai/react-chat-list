f2d2a1f2e48bf58bb163067661d8d52f6de8c46b
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-19 03:22:00 -0700

    :cyclone: Export the rest of images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,22 @@
-import { img_1 } from "./paths"
-export { img_1 }
+import NotFound, {
+    img_1,
+    img_2,
+    img_3,
+    img_4,
+    img_5,
+    img_6,
+    img_7,
+    img_8
+} from "./paths"
+
+export {
+    NotFound,
+    img_1,
+    img_2,
+    img_3,
+    img_4,
+    img_5,
+    img_6,
+    img_7,
+    img_8,
+}
