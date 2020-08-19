0fc641e15001663c813416fd0f2b6cc43cfe00a7
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-19 05:08:46 -0700

    :tada: Create build folder

---
+++ build/precache-manifest.fd130441c5c20dbf035339ef8d4637ed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e82496b96f3d025da0b16b5759a5cc",
+    "url": "/react-chat-list/index.html"
+  },
+  {
+    "revision": "ec72a27f4ae63e82a003",
+    "url": "/react-chat-list/static/css/main.9d8e3c89.chunk.css"
+  },
+  {
+    "revision": "b69ea298abf37558aa13",
+    "url": "/react-chat-list/static/js/2.0645491d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-chat-list/static/js/2.0645491d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec72a27f4ae63e82a003",
+    "url": "/react-chat-list/static/js/main.715de093.chunk.js"
+  },
+  {
+    "revision": "979c86e469bca7e12368",
+    "url": "/react-chat-list/static/js/runtime-main.5e52b9b2.js"
+  },
+  {
+    "revision": "57caa082399c87ce150f0fdfc0462f4d",
+    "url": "/react-chat-list/static/media/img-1.57caa082.jpg"
+  },
+  {
+    "revision": "8ecf5732617626979f691eddb79ec992",
+    "url": "/react-chat-list/static/media/img-2.8ecf5732.jpg"
+  },
+  {
+    "revision": "7902ff83a0bb806897112296e6a74127",
+    "url": "/react-chat-list/static/media/img-3.7902ff83.jpg"
+  },
+  {
+    "revision": "d6dbc61e287b1d4d9aa84cdcaf3b2250",
+    "url": "/react-chat-list/static/media/img-4.d6dbc61e.jpg"
+  },
+  {
+    "revision": "8cf2a8d82823dd054376ef779f3ea10d",
+    "url": "/react-chat-list/static/media/img-5.8cf2a8d8.jpg"
+  },
+  {
+    "revision": "5d205cbaa5545e23974d9b86562f2197",
+    "url": "/react-chat-list/static/media/img-6.5d205cba.jpg"
+  }
+]);
