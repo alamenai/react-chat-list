c56f2af6c5a935d17ea028bcf28cda1f10fb84ad
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-19 03:11:33 -0700

    :cyclone: Connect ContactList to Redux store

--- src/components/features/Contact/List/List.js
+++ src/components/features/Contact/List/List.js
@@ -1,17 +1,46 @@
 import React from "react"
+import { connect } from "react-redux"
 import ContactCard from "../Card"
+import { MessageContext } from "../Message"
+import { NotFound } from "../../../../constants"
 
-const ContactList = () => {
-    return <div className="my-4">
-        <ContactCard isNewMessage={true} />
-        <ContactCard isNewMessage={false} />
-        <ContactCard isNewMessage={false} />
-        <ContactCard isNewMessage={false} />
-        <ContactCard isNewMessage={false} />
-        <ContactCard isNewMessage={false} />
-        <ContactCard isNewMessage={false} />
-        <ContactCard isNewMessage={false} />
-    </div>
+const ContactList = ({ messages }) => {
+
+    const mapContactToList = () => messages.map(message => renderContactCard(message))
+
+    return (<div className="my-4">
+        {messages.length === 0 ? renderNotFound() : mapContactToList()}
+    </div>)
 }
 
-export default ContactList
+const mapStateToProps = (state) => ({ messages: state })
+
+const renderNotFound = () => (
+    <div className="w-full h-64 flex flex-col items-center justify-center">
+        <img src={NotFound} width="60px" height="60px" />
+        <p className="font-hairline mt-4 w-2/3 text-center">
+            There's no contact with the name you provided</p>
+    </div>
+)
+
+const renderContactCard = (message) => {
+    const {
+        contactName,
+        markedAsRead,
+        contactPhoto,
+        time,
+        text
+    } = message;
+
+    return (< MessageContext.Provider value={{
+        contactName,
+        contactPhoto,
+        time,
+        text,
+        markedAsRead,
+    }}>
+        <ContactCard />
+    </MessageContext.Provider >)
+}
+
+export default connect(mapStateToProps)(ContactList);
