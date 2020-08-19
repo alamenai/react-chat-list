8c79910b0076bed8a0b85aed1f57c0e162dca3d9
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-19 03:19:26 -0700

    :cyclone: Dispatch filterContactList action

--- src/components/features/Contact/Search/Search.js
+++ src/components/features/Contact/Search/Search.js
@@ -1,14 +1,23 @@
 import React from "react"
 import TextfieldDefault from "../../../lib/Textfield"
+import { connect } from "react-redux"
+import { fitlerContactList } from "../../../../redux/actions/creators"
 
-const SearchContact = () => {
+const SearchContact = ({ fitlerContactList }) => {
+
+    const handleFilerContactList = (e) => {
+        const contactName = e.target.value
+        fitlerContactList(contactName);
+    }
+
     return (<div className="px-4">
         <TextfieldDefault
             type="text"
             value={null}
             placeholder="Search contact"
-            classes="font-medium" />
+            classes="font-medium"
+            event={handleFilerContactList} />
     </div>)
 }
 
-export default SearchContact
+export default connect(null, { fitlerContactList })(SearchContact)
