3a41c43d83f31f8ce481081fbf200c98076b6252
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-19 03:12:17 -0700

    :tada: Create state.js for holding List state

---
+++ src/components/features/Contact/List/state.js
@@ -0,0 +1,53 @@
+import * as CONTACT_PHOTOS from "../../../../constants"
+
+export const messages = [
+    {
+        contactName: "Monica",
+        contactPhoto: CONTACT_PHOTOS.img_1,
+        time: "21:00",
+        text: "Hi how are you doing?",
+        markedAsRead: false
+    }, {
+        contactName: "Alex",
+        contactPhoto: CONTACT_PHOTOS.img_2,
+        time: "21:00",
+        text: "Hi how are you doing?",
+        markedAsRead: true
+    }, {
+        contactName: "Edward",
+        contactPhoto: CONTACT_PHOTOS.img_3,
+        time: "21:00",
+        text: "I'll call you later",
+        markedAsRead: true
+    }, {
+        contactName: "Clara",
+        contactPhoto: CONTACT_PHOTOS.img_4,
+        time: "21:00",
+        text: "At what time?",
+        markedAsRead: true
+    }, {
+        contactName: "Jane",
+        contactPhoto: CONTACT_PHOTOS.img_5,
+        time: "21:00",
+        text: "I'm going now",
+        markedAsRead: true
+    }, {
+        contactName: "Lina",
+        contactPhoto: CONTACT_PHOTOS.img_6,
+        time: "21:00",
+        text: "Which movie?",
+        markedAsRead: true
+    }, {
+        contactName: "Jessica",
+        contactPhoto: CONTACT_PHOTOS.img_7,
+        time: "21:00",
+        text: "Where are you from?",
+        markedAsRead: true
+    }, {
+        contactName: "Pierre",
+        contactPhoto: CONTACT_PHOTOS.img_8,
+        time: "21:00",
+        text: "Done",
+        markedAsRead: true
+    }
+]
