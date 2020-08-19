8c24c8b638f01d9f2a22b3f305b64f1635c54a95
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-19 03:09:42 -0700

    :wastebasket: Remove MessageContext

--- src/components/features/Contact/Card/Card.js
+++ src/components/features/Contact/Card/Card.js
@@ -1,15 +1,12 @@
 import React from "react"
-import { MessageContext } from "../Message"
 import ContactCardLayout from "../../../../layouts/ContactCardLayout"
 import ContactAvatar from "../Avatar"
 import MessageContent from "../Message"
 
-const ContactCard = ({ isNewMessage }) => (
-    <MessageContext.Provider value={{ isNewMessage: isNewMessage }}>
-        <ContactCardLayout
-            left={<ContactAvatar />}
-            right={<MessageContent />} />
-    </MessageContext.Provider>
+const ContactCard = () => (
+    <ContactCardLayout
+        left={<ContactAvatar />}
+        right={<MessageContent />} />
 )
 
 export default ContactCard
