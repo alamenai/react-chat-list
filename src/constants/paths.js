74f80ef63dd4f6a4a8641ef42655a9d100097ff2
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-19 03:20:53 -0700

    :cyclone: Import the rest of images of contacts

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,5 +1,44 @@
-import Image_1 from "../assets/img/_img-1.jpg"
+import Image_1 from "../assets/img/img-1.jpg"
+import Image_2 from "../assets/img/img-2.jpg"
+import Image_3 from "../assets/img/img-3.jpg"
+import Image_4 from "../assets/img/img-4.jpg"
+import Image_5 from "../assets/img/img-5.jpg"
+import Image_6 from "../assets/img/img-6.jpg"
+import Image_7 from "../assets/img/img-7.jpg"
+import Image_8 from "../assets/img/img-8.jpg"
+import NotFound from "../assets/img/404/404.png"
+
+export default NotFound;
+
 export const img_1 = {
     url: Image_1,
     alt: "Contact 1"
+}
+export const img_2 = {
+    url: Image_2,
+    alt: "Contact 2"
+}
+export const img_3 = {
+    url: Image_3,
+    alt: "Contact 3"
+}
+export const img_4 = {
+    url: Image_4,
+    alt: "Contact 4"
+}
+export const img_5 = {
+    url: Image_5,
+    alt: "Contact 5"
+}
+export const img_6 = {
+    url: Image_6,
+    alt: "Contact 6"
+}
+export const img_7 = {
+    url: Image_7,
+    alt: "Contact 7"
+}
+export const img_8 = {
+    url: Image_8,
+    alt: "Contact 8"
 }
