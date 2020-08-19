2511889d8b8a846b62a86a47f46ed67d0887f956
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-19 03:03:41 -0700

    :wastebasket: Remove width and height from img

--- src/components/lib/Avatar.jsx
+++ src/components/lib/Avatar.jsx
@@ -2,11 +2,11 @@ import React from "react"
 import PropTypes from "prop-types"
 
 const Avatar = ({ src, alt, classes }) => {
-    return <img src={src} alt={alt} width="50px" height="50px" className={classes} />
+    return <img src={src} alt={alt} className={classes} />
 }
 
 const AvatarDefault = (props) => {
-    let defaultClasses = "rounded-full"
+    let defaultClasses = "rounded-full w-12 h-12 object-cover"
     return renderAvatar(props, defaultClasses)
 }
 const renderAvatar = (props, defaultClasses) => {
@@ -15,8 +15,8 @@ const renderAvatar = (props, defaultClasses) => {
 }
 
 Avatar.propTypes = {
-    src: PropTypes.string,
-    alt: PropTypes.string
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
 }
 
 export default AvatarDefault
