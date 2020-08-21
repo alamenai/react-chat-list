90178ad92f52e7a16686b73bfa53dd8391a23cef
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-21 12:59:05 -0700

    :cyclone: Add min-h-screen class for fixing the height of Sidebar

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Sidebar = ({ children }) => {
-    return <div className="bg-white py-4
+    return <div className="bg-white py-4 min-h-screen
                            xs:w-screen sm:w-2/5
                            md:max-w-xs lg:max-w-xs xl:max-w-xs">
         {children}
