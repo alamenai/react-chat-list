e5bdf29be227ffb5064874ae8113fa7f6c3924fe
MenaiAla <ed.sheeran.eng@gmail.com>
2020-08-21 07:55:42 -0700

    :cyclone: Add responsive classes (xs,sm)

--- src/components/features/Contact/Name/Name.js
+++ src/components/features/Contact/Name/Name.js
@@ -6,7 +6,7 @@ const Name = () => {
     const context = useContext(MessageContext);
     return (< Text
         content={context.contactName}
-        classes="text-base font-sans font-medium text-gray-800" />)
+        classes="text-base font-sans font-medium text-gray-800 xs:text-sm sm:text-sm" />)
 }
 
 export default Name
